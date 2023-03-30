879bcd88c6a48624c4ad62699aa47e9d2f847901
Dima_Yenich <nich.dima2020@gmail.com>
2023-03-30 21:54:45 +0300

    last

---
+++ G/H.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G
+{
+    internal class H
+    {
+        static void Main(string[] args)
+        {
+            int time = 0;
+            int number = int.Parse(Console.ReadLine());
+            int[] krots = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Array.Sort(krots);
+
+            int[] nori = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Array.Sort(nori);
+
+            for (int i = 0; i < number; i++)
+            {
+                int dist = Math.Abs(krots[i] - nori[i]);
+                if (dist > time)
+                {
+                    time = dist;
+                }
+            }
+            Console.WriteLine(time);
+        }
+    }
+}
