eb19584d866e701b32f7b08e5d7236e785d796e0
Dima_Yenich <nich.dima2020@gmail.com>
2023-03-30 21:57:33 +0300

    B

---
+++ AA/AA/BB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA
+{
+    internal class BB
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            if (firstNumber == (secondNumber + thirdNumber) || secondNumber == (firstNumber + thirdNumber) || thirdNumber == (firstNumber + secondNumber))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
---
+++ AA/AA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перше число: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Друге число: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Третє число: ");
+            int thirdNumber = int.Parse(Console.ReadLine());
+            if (firstNumber == (secondNumber + thirdNumber) || secondNumber == (firstNumber + thirdNumber) || thirdNumber == (firstNumber + secondNumber))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+                Console.WriteLine("No");
+            Console.ReadLine();
+        }
+    }
+}
